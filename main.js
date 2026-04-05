f88cb908ec694cb115c09a77a443c6bb33ad0e03
Ho Duc Viet <duc.vietho4@gmail.com>
2026-04-05 20:49:51 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,35 +1,73 @@
-//TODO add imports if needed
-//import { exMain } from "./exclude/exampleAss2.js"
-//TODO add/change doc as needed
-/**
- * TODO - Write functional code for this application. You can call any other function, but usage of ".toString(numberSystem)" and "Number.parseInt(number, numberSystem)" is forbidden (only permitted when used on individual digits).
- * The main function which calls the application. 
- * TODO - Please, add specific description here for the application purpose.
- * @param {string} inputNumber number that is being converted
- * @param {number} inputNumberSystem numerical system that the inputNumber is being converted from
- * @param {number} outputNumberSystem numerical system that the inputNumber is being converted into
- * @returns {string} containing number converted to output system
- */
-export function main(inputNumber, inputNumberSystem, outputNumberSystem) {
-  //TODO code
-  //let dtoOut = exMain(inputNumber, inputNumberSystem, outputNumberSystem);
-  return dtoOut;
+/* 
+Vítejte na mém projektu: Kalkulačka pro převod dekadických čísel na binární.
+
+Jak program spustit:
+1. Otevřete terminál / konzoli.
+2. Zadejte příkaz: node domaci_ukol.js nebo npm start (pokud máte nastaveno v package.json) a stiskněte Enter.
+3. Stiskněte Enter.
+
+Jak program funguje:
+- Program vás vyzve k zadání dekadického (desítkového) čísla.
+- Po zadání čísla a potvrzení klávesou Enter se číslo převede na binární tvar.
+- Výsledek se zobrazí v konzoli.
+
+Ošetření chyb:
+- Pokud zadáte neplatný vstup (např. text), program vás upozorní, že se nejedná o číslo.
+- Pokud zadáte záporné číslo, program vás vyzve k zadání kladného čísla.
+
+Užijte si používání této jednoduché kalkulačky!
+*/
+
+console.log("Program se spouští...");
+
+/* Načtení modulu pro práci se vstupem z konzole */
+const readline = require("readline");
+
+/* Vytvoření rozhraní pro čtení a zápis v konzoli */
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+/* 
+Funkce pro převod dekadického čísla na binární:
+- Pokud je číslo 0, vrací "0"
+- Pokud je číslo záporné, vrací chybovou hlášku
+- Jinak provádí převod pomocí dělení dvěma
+*/
+export function decToBin(number) {
+  if (number === 0) return "0";
+  if (number < 0) return "Zadej kladné číslo!";
+
+  let binary = "";
+
+  /* Opakované dělení čísla dvěma a skládání zbytku */
+  while (number > 0) {
+    let remainder = number % 2;
+    binary = remainder + binary;
+    number = Math.floor(number / 2);
+  }
+
+  return binary;
 }
 
-/**
- * TODO - Change this to contain all input number systems that your application can convert from.
- * Function which returns which number systems are permitted on input.
- * @returns {Array} array of numbers refering to permitted input systems
- */
-export function permittedInputSystems() {
-	return [10, 2];
-}
+/* 
+Dotaz na uživatele:
+- Načte vstup z konzole
+- Pokusí se ho převést na číslo
+- Zkontroluje platnost vstupu
+*/
+rl.question("Zadej dekadické číslo: ", (input) => {
+  let number = parseInt(input);
 
-/**
- * TODO - Change this to contain all output number systems that your application can convert to.
- * Function which returns which number systems are permitted on output.
- * @returns {Array} array of numbers refering to permitted output systems
- */
-export function permittedOutputSystems() {
-	return [10, 2];
-}
+  /* Kontrola, zda je vstup číslo */
+  if (isNaN(number)) {
+    console.log("To není číslo!");
+  } else {
+    /* Výpis převedeného binárního čísla */
+    console.log("Binární číslo:", decToBin(number));
+  }
+
+  /* Ukončení rozhraní */
+  rl.close();
+});
